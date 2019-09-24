85a68c5dac1216be6d5d28061f976c36cd412a09
AndrewMosk <moskaletsandrey@yandex.ru>
2019-09-24 16:54:07 +0300

    продолжаю выполение дз

--- Java_core-advanced_level/src/Lesson_3/Main/FirstTask/MainFirst.java
+++ Java_core-advanced_level/src/Lesson_3/Main/FirstTask/MainFirst.java
@@ -1,8 +1,17 @@
 package Lesson_3.Main.FirstTask;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainFirst {
     public static void main(String[] args) {
-        //field south figure garage birth calm under bell yard vital break change
+        String[] stringsArray = {"field", "south", "figure", "garage", "south", "field", "birth", "calm", "under", "bell", "yard", "vital", "south", "break", "break", "change"};
+        Map<String, Integer> hm = new HashMap<>();
 
+        for (String string: stringsArray) {
+            Integer current = hm.get(string);
+            hm.put(string,current == null ? 1: current + 1);
+        }
+        System.out.println(hm);
     }
 }
--- Java_core-advanced_level/src/Lesson_3/Main/SecondTask/MainSecond.java
+++ Java_core-advanced_level/src/Lesson_3/Main/SecondTask/MainSecond.java
@@ -2,6 +2,19 @@ package Lesson_3.Main.SecondTask;
 
 public class MainSecond {
     public static void main(String[] args) {
+        PhoneDictionary phoneDictionary = new PhoneDictionary(PhoneDictionary.initDefaultPhoneDictionary());
 
+        try {
+            phoneDictionary.add("Tom", "1234567","2225551");
+            phoneDictionary.add("Jerry");
+        }catch (MyNumberFormatException e){
+            e.printStackTrace();
+        }
+
+
+        String[] checkArray = {"Tom","Michael","Jerry","Alan"};
+        for (String name: checkArray) {
+            System.out.println(phoneDictionary.get(name));
+        }
     }
 }
