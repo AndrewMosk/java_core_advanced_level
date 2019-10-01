aefaa21c2f66596ce06a39826c22c4b7b82574a8
AndrewMosk <moskaletsandrey@yandex.ru>
2019-10-01 16:22:47 +0300

    Закончил дз5

--- Java_core-advanced_level/src/Lesson_5/MainThread.java
+++ Java_core-advanced_level/src/Lesson_5/MainThread.java
@@ -1,129 +1,92 @@
 package Lesson_5;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class MainThread {
-    static final int size = 10000000;
-    static final int threadNumber = 4;
-    static final int h = size / threadNumber;
+    private static final int size = 10000000;
+    private static final int threadNumber = 4;
+    private static final int h = size / threadNumber;
 
     public static void main(String[] args) {
-        float[] originalArray = initArray();
-        System.out.println("Вычисления в один поток завершились за " +  singleThread(originalArray));
+        Result resultSingleThread = singleThread(initArray());
+        Result resultMultiThread = multiThread(initArray());
 
-        System.out.println("Вычисления в несколько потоков завершились за " +  multiThread(originalArray));
+        if (Arrays.equals(resultSingleThread.getResultArray(),resultMultiThread.getResultArray())) {
+            System.out.println("Вычисления в один поток завершились за " + resultSingleThread.getExecutionTime() + " сек.");
+            System.out.println("Вычисления в " + threadNumber + " потока(ов) завершились за " + resultMultiThread.getExecutionTime() + " сек.");
+        }else System.out.println("Упс... что-то пошло не так и массивы не равны :-(");
     }
 
-    private static double singleThread(float[] originalArray){
+    private static Result singleThread(float[] originalArray){
         long startTime = System.currentTimeMillis();
 
         for (int i=0; i<originalArray.length; i++){
-            originalArray[i] = (float)(originalArray[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            originalArray[i] = (float)(originalArray[i] * Math.sin(0.2f + i / 5.0) * Math.cos(0.2f + i / 5.0) * Math.cos(0.4f + i / 2.0));
         }
 
         long finishTime = System.currentTimeMillis();
         double executionTime = ((double)finishTime-(double)startTime)/1000.0;
 
-        return executionTime;
+        return new Result(executionTime, originalArray);
     }
 
-    private static double multiThread1(float[] originalArray){
-        //коллеккция с ключом. массив таких коллекций?
+    private static Result multiThread(float[] originalArray){
         long startTime = System.currentTimeMillis();
 
-        float[] array1 = new float[h];
-        float[] array2 = new float[h];
-        System.arraycopy(originalArray, 0, array1, 0, h);
-        System.arraycopy(originalArray, h, array2, 0, h);
+        //двумерный массив для приема вычислений
+        float[][] outputArrays = new float[threadNumber][h];
 
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                calculateArray(array1);
-            }
-        });
+        HashSet<Thread> threadHashSet = new HashSet<>();
 
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                calculateArray(array2);
-            }
-        });
+        //делю массив, создаю потоки
+        for (int i=0, startPos = 0; i<threadNumber; i++, startPos += h){
+            float[] array = outputArrays[i]; //массив приемник
+            System.arraycopy(originalArray, startPos, array, 0, h);
 
-        t1.start();
-        t2.start();
-
+            int finalStartPos = startPos;
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() { calculateArray(array, finalStartPos); }
+            });
+            threadHashSet.add(t);
+        }
+        //старт потоков
+        Iterator<Thread> iteratorStart = threadHashSet.iterator();
+        while (iteratorStart.hasNext()){
+            Thread t = iteratorStart.next();
+            t.start();
+        }
+        //join потоков
+        Iterator<Thread> iteratorJoin = threadHashSet.iterator();
         try {
-            t1.join();
-            t2.join();
+            while (iteratorJoin.hasNext()){
+                Thread t = iteratorJoin.next();
+                t.join();
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        //обратная склейка
+        for (int i =0, startPos = 0; i<threadNumber; i++, startPos +=h){
+            System.arraycopy(outputArrays[i], 0, originalArray, startPos, h);
 
-        System.arraycopy(array1, 0, originalArray, 0, h);
-        System.arraycopy(array1, 0, originalArray, h, h);
-
+        }
         long finishTime = System.currentTimeMillis();
         double executionTime = ((double)finishTime-(double)startTime)/1000.0;
 
-        return executionTime;
+        return new Result(executionTime, originalArray);
     }
 
-
-
-
-    private static double multiThread(float[] originalArray){
-        long startTime = System.currentTimeMillis();
-
-        float[] array1 = new float[h];
-        float[] array2 = new float[h];
-        System.arraycopy(originalArray, 0, array1, 0, h);
-        System.arraycopy(originalArray, h, array2, 0, h);
-
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                calculateArray(array1);
-            }
-        });
-
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                calculateArray(array2);
-            }
-        });
-
-        t1.start();
-        t2.start();
-
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    private static void calculateArray(float[] array, int startPos){
+        for (int i =0; i<array.length; i++, startPos++){
+            array[i] = (float)(array[i] * Math.sin(0.2f + startPos / 5.0) * Math.cos(0.2f + startPos / 5.0) * Math.cos(0.4f + startPos / 2.0));
         }
-
-        System.arraycopy(array1, 0, originalArray, 0, h);
-        System.arraycopy(array1, 0, originalArray, h, h);
-
-        long finishTime = System.currentTimeMillis();
-        double executionTime = ((double)finishTime-(double)startTime)/1000.0;
-
-        return executionTime;
-    }
-
-    private static float[] calculateArray(float[] array){
-        for (int i=0; i<array.length; i++){
-            array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-        return array;
     }
 
     private static float[] initArray(){
-        float[] arr = new float[size];
-        Arrays.fill(arr, 1);
+        float[] array = new float[size];
+        Arrays.fill(array, 1.0f);
 
-        return arr;
+        return array;
     }
 }
